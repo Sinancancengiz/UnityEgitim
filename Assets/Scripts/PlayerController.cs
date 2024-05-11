c9cc7d9e8347bfbcf37b1af150c6273af604fb07
sccengizlrn <sccengizlrn@gmail.com>
2024-05-11 14:36:18 +0300

    Player Idle and Running Animations

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,10 +7,15 @@ public class PlayerController : MonoBehaviour
     public float movementSpeed = 5f;    // hareket hýzý
     public float rotationSpeed = 10f;
     private Rigidbody rb;   // fizik iþlemleri için rigidboy'i tanýmladýk
+    private Animator animator;  // Animasyon iþlemleri için
+
 
     private void Start()
     {
         rb = GetComponent<Rigidbody>(); // Ayný GameObject üzerindeki rigidbody'e ulaþmak için GetComponent fonksiyonu kullanýldý
+        // Ulaþmak için GetComponent fonksiyonu
+
+        animator = GetComponent<Animator>();
     }
 
     private void Update()
@@ -22,9 +27,13 @@ public class PlayerController : MonoBehaviour
         // X ve Y ekseni üzerindeki hareket giriþleri X ve Z eksenleri olmak üzere dönüþtürüldü.
         var movementDirection = new Vector3(horizontal, 0, vertical);
 
+        animator.SetBool("isRunning", movementDirection != Vector3.zero);   // Ya bu ya da alttaki
+        //animator.SetBool("isRunning", rb.velocity != Vector3.zero);   // Ýkisinden birisi
+
         if (movementDirection == Vector3.zero)
         {
             Debug.Log("Input yok");
+            rb.velocity = Vector3.zero;
             return;
         }
 
